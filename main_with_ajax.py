ebf2ac896015416c99b77bc926fc8548eb80ad56
Igor Garmatenko <igoren92@gmail.com>
2022-05-11 23:26:12 +0300

    remove some files. add random user-aget

--- main_with_ajax.py
+++ main_with_ajax.py
@@ -1,18 +1,12 @@
-import time
 import requests
 from bs4 import BeautifulSoup
 import dbConnect
-import pymongo
-import send_telegram
-from datetime import datetime
-
+from user_agent import generate_user_agent
 
 
 TOKEN = '1799874715:AAE0Rj4-SpV-OMAxLZg3hhbKIIueYwXZ9oM'
 CHANNEL = -1001337710941
-HEADERS = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
-    'accept': '*/*'}
+
 
 def save2db(collection, data):
 
@@ -24,17 +18,18 @@ def select_from_db(collection, query):
 
 # this function returns HTML page code
 def get_html(url, params=None):
-
+    ua = generate_user_agent()
+    headers = {'User-Agent': ua}
     try:
-        response = requests.get(url, headers=HEADERS, params=params).text
+        response = requests.get(url, headers=headers, params=params).text
     except:
-        response = ''
+        response = '123'
+
     return response
 
 
 # this function returns a list of links to matches from this html page
 def get_match_list(html):
-
     soup = BeautifulSoup(html, 'html.parser')
     items = soup.find_all('div', class_='block-item iscroll-item has-image')
     links = []
@@ -66,7 +61,7 @@ def parser():
 
     bets = []
     for i in range(4):
-        url = 'https://bookmaker-ratings.com.ua/ru/tips/tomorrow/?paged={}'.format(str(i+1))
+        url = 'https://bookmaker-ratings.com.ua/tips/tomorrow/?paged={}'.format(str(i+1))
         match_list = get_match_list(get_html(url))
         print(url)
         print(match_list)
@@ -78,27 +73,28 @@ def parser():
 
 if __name__ == '__main__':
 
-    time1 = time.time()
+    # time1 = time.time()
     bets = parser()
-    collection_name = datetime.now().strftime("%d-%m-%Y")
-    try:
-        client = pymongo.MongoClient('localhost', 27017)
-        db = client['pydbtest']
-        col = db[collection_name]
-    except:
-        print('Connection to database error.')
+    print(bets)
+    # collection_name = datetime.now().strftime("%d-%m-%Y")
+    # try:
+    #     client = pymongo.MongoClient('localhost', 27017)
+    #     db = client['pydbtest']
+    #     col = db[collection_name]
+    # except:
+    #     print('Connection to database error.')
+    #
+    # save2db(col, bets)
 
-    save2db(col, bets)
-
-    sms = """
-        Ставка по линии:
-
-        """
-    inf = select_from_db(col, {'kindOfSport': 'футбол'})
-    sms += inf['title'] + ': ' + inf['bet']
-
-    print(sms)
-    send_telegram.write_message(TOKEN, CHANNEL, sms)
-    time2 = time.time()
-    print('Work time: {}'.format(time2 - time1))
+    # sms = """
+    #     Ставка по линии:
+    #
+    #     """
+    # # inf = select_from_db(col, {'kindOfSport': 'футбол'})
+    # sms += inf['title'] + ': ' + inf['bet']
+    #
+    # print(sms)
+    # # send_telegram.write_message(TOKEN, CHANNEL, sms)
+    # time2 = time.time()
+    # print('Work time: {}'.format(time2 - time1))
 
--- send_telegram.py
+++
@@ -1,8 +0,0 @@
-from requests import get
-
-def write_message(TOKEN, channel_id, data):
-
-    get('https://api.telegram.org/bot{}/sendMessage'.format(TOKEN), params=dict(
-        chat_id=channel_id,
-        text=data
-    ))
