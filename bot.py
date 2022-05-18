aa49487b05aebb4080d7c7482c4cb46f4a3a8433
Igor Garmatenko <igoren92@gmail.com>
2022-05-18 14:54:08 +0300

    refactor bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import json
 from datetime import datetime
+import random
 import logging
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
@@ -13,14 +14,13 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-tips_today = InlineKeyboardButton('Прогнозы на сегодня!', callback_data='tips_today')
-tips_tommorow = InlineKeyboardButton('Прогнозы на завтра!', callback_data='tips_tommorow')
-tips_all = InlineKeyboardButton('Все что есть!', callback_data='tips_all')
-kb_date = InlineKeyboardMarkup().add(tips_today, tips_tommorow, tips_all)
 
-tip_one = InlineKeyboardButton('Один прогноз!', callback_data='one')
-tip_all = InlineKeyboardButton('Давай все!', callback_data='all')
-kb_numb = InlineKeyboardMarkup().add(tip_one, tip_all)
+tip_one_today = InlineKeyboardButton('Сегодня - Один прогноз!', callback_data='one_today')
+tip_all_today = InlineKeyboardButton('Сегодня - Давай все!', callback_data='all_today')
+tip_one_tomorrow = InlineKeyboardButton('Завтра - Один прогноз!', callback_data='one_tomorrow')
+tip_all_tomorrow = InlineKeyboardButton('Завтра - Давай все!', callback_data='all_tomorrow')
+tip_all = InlineKeyboardButton('Давай все что есть!', callback_data='all')
+kb = InlineKeyboardMarkup().add(tip_one_today, tip_all_today, tip_one_tomorrow, tip_all_tomorrow, tip_all)
 
 
 def get_tip(day='today', count = 'one'):
@@ -30,64 +30,89 @@ def get_tip(day='today', count = 'one'):
         return data
     if day == 'today':
         tips = [item for item in data if item['date'].split('/')[1].split(' ')[0] == str(datetime.now().day)]
-        print(tips)
-        return tips
+        if count == 'all':
+            return tips
+        if count =='one':
+            return tips[random.randint(0, len(tips))]
     if day == 'tomorrow':
         tips = [item for item in data if item['date'].split('/')[1].split(' ')[0] == str(datetime.now().day + 1)]
-        print(tips)
-        return tips
+        if count == 'all':
+            return tips
+        if count == 'one':
+            return tips[random.randint(0, len(tips))]
 
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await bot.send_message(message.chat.id, text="Привет. Я подберу для тебя лучшие прогнозы на спорт.", reply_markup=kb_date)
-
-
-@dp.callback_query_handler(lambda c: c.data == 'tips_today')
-async def process_callback_button1(callback_query: types.CallbackQuery):
-    await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(callback_query.from_user.id, 'На сегодня!')
-
-    tips = get_tip(day='today')
-    for tip in tips:
-        msg = f'----Прогноз:----\n' \
-              f'Матч: {tip["title"]}\n' \
-              f'Дата: {tip["date"]}\n' \
-              f'Ставка: {tip["bet"]}\n' \
-              f'Коэф: {tip["kf"]}\n'
-        await bot.send_message(callback_query.from_user.id, msg)
-
-
-@dp.callback_query_handler(lambda c: c.data == 'tips_tommorow')
-async def process_callback_button1(callback_query: types.CallbackQuery):
-    await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(callback_query.from_user.id, 'На завтра', reply_markup=kb_numb)
-
-
-@dp.callback_query_handler(lambda c: c.data == 'tips_all')
-async def process_callback_button1(callback_query: types.CallbackQuery):
-    await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(callback_query.from_user.id, 'Все')
-    tips = get_tip(day='all')
-    for tip in tips:
-        msg = f'----Прогноз:----\n' \
-              f'Матч: {tip["title"]}\n' \
-              f'Дата: {tip["date"]}\n' \
-              f'Ставка: {tip["bet"]}\n' \
-              f'Коэф: {tip["kf"]}\n'
-        await bot.send_message(callback_query.from_user.id, msg)
-
-
-@dp.callback_query_handler(lambda c: c.data == 'one')
-async def process_callback_button1(callback_query: types.CallbackQuery):
-    await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(callback_query.from_user.id, 'Вот тебе один прогноз')
+    await bot.send_message(message.chat.id, text="Привет. Я подберу для тебя лучшие прогнозы на спорт.", reply_markup=kb)
 
 
 @dp.callback_query_handler(lambda c: c.data == 'all')
 async def process_callback_button1(callback_query: types.CallbackQuery):
     await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(callback_query.from_user.id, 'Вот тебе все прогнозы')
+    await bot.send_message(callback_query.from_user.id, 'Все')
+    tips = get_tip(day='all', count='all')
+    for tip in tips:
+        msg = f'----Прогноз:----\n' \
+              f'Матч: {tip["title"]}\n' \
+              f'Дата: {tip["date"]}\n' \
+              f'Ставка: {tip["bet"]}\n' \
+              f'Коэф: {tip["kf"]}\n'
+        await bot.send_message(callback_query.from_user.id, msg)
+
+
+@dp.callback_query_handler(lambda c: c.data == 'one_today')
+async def process_callback_button1(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, 'Вот тебе один прогноз на сегодня')
+    tip = get_tip(day='today', count='one')
+    msg = f'---------Прогноз:--------\n' \
+        f'Матч: {tip["title"]}\n' \
+        f'Дата: {tip["date"]}\n' \
+        f'Ставка: {tip["bet"]}\n' \
+        f'Коэф: {tip["kf"]}\n'
+    await bot.send_message(callback_query.from_user.id, msg)
+
+
+@dp.callback_query_handler(lambda c: c.data == 'all_today')
+async def process_callback_button1(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, 'Вот тебе все прогнозы на сегодня')
+    tips = get_tip(day='today', count='all')
+    for tip in tips:
+        msg = f'---------Прогноз:--------\n' \
+              f'Матч: {tip["title"]}\n' \
+              f'Дата: {tip["date"]}\n' \
+              f'Ставка: {tip["bet"]}\n' \
+              f'Коэф: {tip["kf"]}\n'
+        await bot.send_message(callback_query.from_user.id, msg)
+
+
+@dp.callback_query_handler(lambda c: c.data == 'one_tomorrow')
+async def process_callback_button1(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, 'Вот тебе один прогноз на сегодня')
+    tip = get_tip(day='tomorrow', count='one')
+    msg = f'---------Прогноз:--------\n' \
+        f'Матч: {tip["title"]}\n' \
+        f'Дата: {tip["date"]}\n' \
+        f'Ставка: {tip["bet"]}\n' \
+        f'Коэф: {tip["kf"]}\n'
+    await bot.send_message(callback_query.from_user.id, msg)
+
+
+@dp.callback_query_handler(lambda c: c.data == 'all_tomorrow')
+async def process_callback_button1(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, 'Вот тебе все прогнозы на сегодня')
+    tips = get_tip(day='tomorrow', count='all')
+    for tip in tips:
+        msg = f'---------Прогноз:--------\n' \
+              f'Матч: {tip["title"]}\n' \
+              f'Дата: {tip["date"]}\n' \
+              f'Ставка: {tip["bet"]}\n' \
+              f'Коэф: {tip["kf"]}\n'
+        await bot.send_message(callback_query.from_user.id, msg)
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 from datetime import datetime
+import random
 
 day = str(datetime.now().day)
 s = '14:00 МСК/15 май'
 
 a = s.split('/')[1].split(' ')[0]
 print(type(a), type(day))
+
+print(random.randint(0,10))
